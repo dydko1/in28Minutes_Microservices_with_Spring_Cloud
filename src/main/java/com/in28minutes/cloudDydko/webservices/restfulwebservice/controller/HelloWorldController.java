439f6cbdeaef94a05396c909aedac2d4d0cba4ea
dydko1 <miroslaw.dyduch@gmail.com>
2021-12-04 08:57:14 +0100

    Lesson 018 finished

--- src/main/java/com/in28minutes/cloudDydko/webservices/restfulwebservice/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/cloudDydko/webservices/restfulwebservice/controller/HelloWorldController.java
@@ -1,19 +1,22 @@
 package com.in28minutes.cloudDydko.webservices.restfulwebservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "hello-world")
+    @GetMapping(path = "hello-world")
     public String helloWorld() {
         return "Hello World !!";
     }
 
-    @RequestMapping(path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World from Bean");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean("Doberek Pani/Panu: " + name);
     }
 }
