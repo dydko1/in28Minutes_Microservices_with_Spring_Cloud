103e08ced19e08febfbab7d81a20b8099c82e028
dydko1 <miroslaw.dyduch@gmail.com>
2021-12-06 20:53:45 +0100

    Lesson 023 finished

--- src/main/java/com/in28minutes/cloudDydko/webservices/restfulwebservice/user/UserResource.java
+++ src/main/java/com/in28minutes/cloudDydko/webservices/restfulwebservice/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.in28minutes.cloudDydko.webservices.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,9 +25,10 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUSer(@RequestBody User user)
-    {
-        service.save(user);
+    public ResponseEntity<Object> createUSer(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }
