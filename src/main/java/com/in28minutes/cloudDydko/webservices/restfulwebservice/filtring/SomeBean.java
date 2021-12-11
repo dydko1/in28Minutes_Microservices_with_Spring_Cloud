1ad400e4f397be4a931c49453419c0d74b54dc01
dydko1 <miroslaw.dyduch@gmail.com>
2021-12-11 08:59:37 +0100

    Lesson 038 finished, JsonIgnore

---
+++ src/main/java/com/in28minutes/cloudDydko/webservices/restfulwebservice/filtring/SomeBean.java
@@ -0,0 +1,43 @@
+package com.in28minutes.cloudDydko.webservices.restfulwebservice.filtring;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(value = {"field1","field2"})
+public class SomeBean {
+    private String field1;
+    private String field2;
+    //@JsonIgnore
+    private String filed3;
+
+    public SomeBean(String field1, String field2, String filed3) {
+        super();
+        this.field1 = field1;
+        this.field2 = field2;
+        this.filed3 = filed3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public String getFiled3() {
+        return filed3;
+    }
+
+    public void setFiled3(String filed3) {
+        this.filed3 = filed3;
+    }
+}
