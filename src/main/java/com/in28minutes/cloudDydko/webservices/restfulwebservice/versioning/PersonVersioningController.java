ada6bfcb3eb881952501a7c314d820b1939c84db
dydko1 <miroslaw.dyduch@gmail.com>
2021-12-12 19:36:56 +0100

    Lesson 041 finished, Header and content negotation

--- src/main/java/com/in28minutes/cloudDydko/webservices/restfulwebservice/versioning/PersonVersioningController.java
+++ src/main/java/com/in28minutes/cloudDydko/webservices/restfulwebservice/versioning/PersonVersioningController.java
@@ -15,4 +15,24 @@ public class PersonVersioningController {
     public PersonV2 personV2() {
         return new PersonV2(new Name("Mireczek", "Dydko"));
     }
+
+    @GetMapping(value = "person/header", headers = "X-API-VERSION=1")
+    public PersonV1 paramV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "person/header", headers = "X-API-VERSION=2")
+    public PersonV2 paramV2() {
+        return new PersonV2(new Name("Miro", "Dydo"));
+    }
+
+    @GetMapping(value = "person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 produceV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 produceV2() {
+        return new PersonV2(new Name("Miro", "Dydo"));
+    }
 }
